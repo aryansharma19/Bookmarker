59da503b8042505436b2f6ad6d6cde3c0995f76c
Aryan Sharma <aryansharmayea@gmail.com>
2022-08-31 16:58:46 +0530

    adding launch class to the Bookmarker package,to test the functionality of the code

---
+++ Bookmarker/Launch.java
@@ -0,0 +1,40 @@
+package Bookmarker;
+
+import Entities.Bookmark;
+import Entities.User;
+import Manager.BookmarkManager;
+import Manager.UserManager;
+
+public class Launch {
+    private static User[] users;
+    private static Bookmark[][] bookmarks;
+    private static void loadData(){
+        System.out.println("1) Loading data . . .  ");
+        DataStore.loadData();
+        users = UserManager.getInstance().getUsers();
+        bookmarks = BookmarkManager.getInstance().getBookmarks();
+
+        System.out.println("Printing data . . . ");
+        printUsers();
+        printBookmarks();
+    }
+    public static void main(String[] args) {
+        loadData();
+    }
+
+    private static void printUsers(){
+        for(User user:users){
+            System.out.println(user);
+        }
+        System.out.println();
+    }
+
+    private static void printBookmarks(){
+        for(Bookmark[] bookType:bookmarks){
+            for(Bookmark book:bookType){
+                System.out.println(book);
+            }
+            System.out.println();
+        }
+    }
+}
