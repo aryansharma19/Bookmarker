e1980253d4d2b5b6aeb7753f5e8e1130704f83e3
Aryan Sharma <aryansharmayea@gmail.com>
2022-08-31 12:46:00 +0530

    Storing all the constant values in seperate classes

---
+++ Constants/Gender.java
@@ -0,0 +1,8 @@
+package Constants;
+
+public class Gender {
+    private Gender(){};
+    private static final int MALE = 0;
+    private static final int FEMALE = 1;
+    private static final int TRANSGENDER = 2;
+}
