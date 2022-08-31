de23d4f35bf8b90902197afb381176e853adb492
Aryan Sharma <aryansharmayea@gmail.com>
2022-08-31 17:30:04 +0530

    making changes in the BookmarkDao class so that it can pass the information of the bookmarked objects to the data base

--- Dao/BookmarkDao.java
+++ Dao/BookmarkDao.java
@@ -2,9 +2,14 @@ package Dao;
 
 import Bookmarker.DataStore;
 import Entities.Bookmark;
+import Entities.UserBookmark;
 
 public class BookmarkDao {
     public Bookmark[][] getBookmarks(){
         return DataStore.getBookmarks();
+    }
+
+    public void saveUserBookmark(UserBookmark userBookmark){
+        DataStore.add(userBookmark);
     }
 }
