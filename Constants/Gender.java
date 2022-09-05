5242dfc38d26c53f5afc94c475a15e6b4d01bc55
Aryan Sharma <aryansharmayea@gmail.com>
2022-09-05 13:26:44 +0530

    updating the gender class

--- Constants/Gender.java
+++ Constants/Gender.java
@@ -2,7 +2,7 @@ package Constants;
 
 public class Gender {
     private Gender(){};
-    private static final int MALE = 0;
-    private static final int FEMALE = 1;
-    private static final int TRANSGENDER = 2;
+    public static final int MALE = 0;
+    public static final int FEMALE = 1;
+    public static final int TRANSGENDER = 2;
 }
